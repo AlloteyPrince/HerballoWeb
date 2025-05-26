2e9351776980c7dd24e83c710ff129e311c8745a
Prince <oscarprince2020@gmail.com>
2025-05-26 15:39:31 -0700

    Renaming and Restructuring

--- Desktop/Code-HerbalAlchemy/Herbal-Alchemy/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-// import Hero from '@/components/Hero.vue'
-
-let vm = createApp(App)
-
-// vm.component("Hero", Hero)
-
-vm.mount('#app')
