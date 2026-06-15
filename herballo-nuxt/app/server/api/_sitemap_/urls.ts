840d87b7661d7a8dcb02cfaed1c62adae892220b
Prince <oscarprince2020@gmail.com>
2026-06-15 10:49:23 +0100

    added some corrections to sitemap

--- herballo-nuxt/app/server/api/_sitemap_/urls.ts
+++ herballo-nuxt/app/server/api/_sitemap_/urls.ts
@@ -1,26 +1,19 @@
 import { createClient } from '@supabase/supabase-js'
-import { defineSitemapEventHandler, asSitemapUrl } from '#sitemap/server'
 
-export default defineSitemapEventHandler(async () => {
+export default defineEventHandler(async () => {
   const client = createClient(
     process.env.SUPABASE_URL!,
     process.env.SUPABASE_KEY!
   )
 
-  // Fetch all published blog posts
-  const { data: blogs } = await client
-    .from('blogs') // 👈 change to your actual table name
+  const { data: posts } = await client
+    .from('blog_posts')
     .select('slug, updated_at')
-    .eq('published', true) // 👈 remove this if you don't have a published column
 
-  return [
-    ...(blogs || []).map((post) =>
-      asSitemapUrl({
-        loc: `/blog/${post.slug}`,
-        lastmod: post.updated_at,
-        changefreq: 'weekly',
-        priority: 0.8,
-      })
-    ),
-  ]
+  return (posts || []).map((post) => ({
+    loc: `/blog/${post.slug}`,
+    lastmod: post.updated_at,
+    changefreq: 'weekly',
+    priority: 0.8,
+  }))
 })
